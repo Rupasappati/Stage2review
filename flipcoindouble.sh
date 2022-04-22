8254a1503e5ad6e36e17d53f475a9ea8c346d4cf
Rupasappati <rupasappati@gmail.com>
2022-04-22 14:00:23 +0530

    Flip coin singlet and doblet

---
+++ flipcoindouble.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+function doublet()
+{
+declare -A coinRes
+coinRes=( ['HH']=0 ['HT']=0 ['TH']=0 ['TT']=0 )
+hh=0
+ht=1
+th=2
+tt=3
+for(( i=1; i<=$num; i++ ))
+do
+	toss=$((RANDOM%4))
+	case $toss in
+	$hh)
+		coinRes['HH']=$((${coinRes['HH']}+1))
+		;;
+	$ht)
+		coinRes['HT']=$((${coinRes['HT']}+1))
+		;;
+	$th)
+		coinRes['TH']=$((${coinRes['TH']}+1))
+		;;
+	*)
+		coinRes['TT']=$((${coinRes['TT']}+1))
+		;;
+	esac
+done
+
+
+hhWinPercentage=$(((${coinRes['HH']}*100)/num))
+htWinPercentage=$(((${coinRes['HT']}*100)/num))
+thWinPercentage=$(((${coinRes['TH']}*100)/num))
+ttWinPercentage=$((100-(hhWinPercentage+htWinPercentage+thWinPercentage)))
+
+dheadPercentage=$(($hhWinPercentage+($thWinPercentage+$htWinPercentage)/2))
+dtailPercentage=$((100-$dheadPercentage))
+
+echo "Doublet head Percentage=$dheadPercentage%"
+echo "Doublet tail Percentage=$((100-$dheadPercentage))%"
+
+
+}
